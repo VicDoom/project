0802371bf77dd391b7f77808c428886b20791230
Nikita <n_k_m1999@mail.ru>
2019-10-21 18:23:16 +0300

    smth to do

---
+++ myjavascript.js
@@ -0,0 +1,41 @@
+// var one = 'Hello there';
+// var two = 'General Kenobi';
+// var three = 'Vzuuum';
+
+// //alert('Hello!');
+// //console.log(one);
+// //document.getElementById('message').innerHTML = one;
+// //$('#test').text(one);
+// //$('#test').fadeOut('slow');
+// $('.myclass').text(one);
+
+// document.getElementsByClassName('myclass')[0].innerHTML = one;
+// document.getElementsByClassName('myclass')[1].innerHTML = two;
+// document.getElementsByClassName('myclass')[2].innerHTML = three;
+
+// console.log(document.getElementsByClassName('myclass')[0]);
+
+// document.getElementsByClassName('myclass')[0].style.color = 'red';
+
+// $('.myclass').css('font-size', '20px');
+
+// //array = ['всем ', 'привет ', 'с вами ', 'Сокол'];
+
+// //array.splice(1, 1, 'здарова, Щеглы ');
+// //arr = array.pop();
+// //arr.push('!');
+// //array.sort();
+
+// //alert(array);
+
+// function starWars (a) {
+//     return "приветствую, " + a;
+// }
+
+// console.log(starWars("Йода"));
+// document.getElementById('test').innerHTML = starWars('Йода');
+
+let dl= 5;
+
+console.log(dl);
+
